affce5f1cc5d23f9fcaddee2253d94a6a31727ef
maruda <weronikajakimowicz2@gmail.com>
2024-04-04 18:19:07 +0200

    04-04-24

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+    public float offset = 5f;
+
+    public void GenerateLabyrinth () {
+        // przechodzimy piksel po pikselu i odpowiednia rzecz umieszczamy na scenie
+        for (int x = 0; x < map.width; x++) {
+            for (int y = 0; y < map.height; y++) GenerateTile(x, y);
+        }
+    }
+
+    private void GenerateTile (int x, int z) {
+        Color pixelColor = map.GetPixel(x, z);
+
+        // czy wybrany pixel jest przeźroczysty?
+        if (pixelColor.a == 0) {
+            return;
+        }
+
+        foreach (ColorToPrefab cM in colorMappings) {
+            if (cM.color.Equals(pixelColor)) {
+                Vector3 position = new Vector3 (x, 0, z) * offset;
+                Instantiate(cM.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
+
+[CustomEditor(typeof(LevelGenerator))]
+public class EditorButton : Editor {
+    public override void OnInspectorGUI()
+    {
+        // rysujemy defaultowy inspektor
+        DrawDefaultInspector();
+
+        LevelGenerator generator = (LevelGenerator)target;
+
+        // dodajemy przycisk i sprawdzamy, czy zosta kliknięty
+        if (GUILayout.Button("Create Labyrinth")) generator.GenerateLabyrinth();
+    }
+}
